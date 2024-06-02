8429036bd29de44777feb2ff0bef45d5b22be973
MatiMa <mati.mannisalu@tktk.ee>
2024-06-02 21:40:53 +0300

    add week4 code

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿string userPIN;
+
+Console.WriteLine("Enter your PIN");
+userPIN = Console.ReadLine();
+while (true)
+{
+    if (userPIN == "1234")
+    {
+        Console.WriteLine("Welcome!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Wrong PIN. Try again.");
+    }
+}
---
+++ Week4Loops/Program.cs
@@ -0,0 +1,9 @@
+﻿
+//FOR-LOOP
+//DRY-principle _Do not Repeat Yourself
+
+
+for(int i = 0; i < 5; i++) //i++ = i+1
+{
+    Console.WriteLine("We don't need no education.");
+}
